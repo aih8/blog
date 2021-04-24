647d741d8085beaaf7dd473df76f3060bc0fb2b8
huziheng <huziheng@mudu.tv>
2021-04-24 11:02:01 +0800

    update blog

--- source/outlet/static/scripts/updateYiqing.js
+++ source/outlet/static/scripts/updateYiqing.js
@@ -1,27 +1,27 @@
-var https = require('https');
-var fs = require('fs');
-var path = require('path');
+import path from "path";
+import https from "https";
+import fs from "fs";
 
 function getRemoteData(url) {
-    return new Promise(function(resolve, reject) {
-        https.get(url, function(res){
-            var html = '';
+  return new Promise(function (resolve, reject) {
+    https.get(url, function (res) {
+      var html = '';
 
-            res.on('data', function (data) {
-                html  += data;
-            });
+      res.on('data', function (data) {
+        html += data;
+      });
 
-            res.on('end', function () {
-                writeFile(path.resolve(__dirname, './yiqing.json'), html, {flag: 'w+'});
-            });
-        }).on('error',function(){
-            console.log('出错！');
-        });
+      res.on('end', function () {
+        writeFile(path.resolve(__dirname, './yiqing.json'), html, {flag: 'w+'});
+      });
+    }).on('error', function () {
+      console.log('出错！');
     });
+  });
 }
 
 function writeFile(pathInfo, data, options = {flag: 'w+'}) {
-    fs.writeFileSync(pathInfo, data, options);
+  fs.writeFileSync(pathInfo, data, options);
 }
 
 getRemoteData('https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5');
