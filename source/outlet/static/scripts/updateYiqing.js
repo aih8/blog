e14072a6fbbbd18133daf07772115cf43c3d7008
huziheng <huziheng@mudu.tv>
2021-04-24 08:20:16 +0800

    update

---
+++ source/outlet/static/scripts/updateYiqing.js
@@ -0,0 +1,29 @@
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+
+function getRemoteData(url) {
+    return new Promise(function(resolve, reject) {
+        https.get(url, function(res){
+            var html = '';
+
+            res.on('data', function (data) {
+                html  += data;
+            });
+
+            res.on('end', function () {
+                writeFile(path.resolve(__dirname, './yiqing.json'), html, {flag: 'w+'});
+                process.exit(1);
+            });
+        }).on('error',function(){
+            console.log('出错！');
+            process.exit(-1);
+        });
+    });
+}
+
+function writeFile(pathInfo, data, options = {flag: 'w+'}) {
+    fs.writeFileSync(pathInfo, data, options);
+}
+
+getRemoteData('https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5');
