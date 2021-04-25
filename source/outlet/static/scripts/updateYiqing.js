b0dafc30b9effaa914379f8fb5d8e2fc37f3d577
胡紫恒 <huzh32510@hundsun.com>
2021-04-25 15:03:25 +0800

    '博客更新'

--- source/outlet/static/scripts/updateYiqing.js
+++ source/outlet/static/scripts/updateYiqing.js
@@ -2,7 +2,20 @@ var https = require('https');
 var fs = require('fs');
 var path = require('path');
 
-function getRemoteData(url) {
+// 疫情json数据的路径
+var staticYiqingJsonPathInfo = path.resolve(__dirname, '../yiqing.json');
+// 获取疫情数据的接口
+var httpUrl = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5';
+
+function getRemoteData(url, filePath) {
+  httpsGet(url).then(function (res) {
+    writeFile(filePath, res, { flag: 'w+' });
+  }, function() {
+    console.log('出错！');
+  });
+}
+
+function httpsGet(url) {
   return new Promise(function (resolve, reject) {
     https.get(url, function (res) {
       var html = '';
@@ -12,18 +25,16 @@ function getRemoteData(url) {
       });
 
       res.on('end', function () {
-        const p = path.resolve(__dirname, '../yiqing.json');
-        writeFile(p, html, {flag: 'w+'});
-        console.log('update 疫情json success。path: ' + p);
+        resolve(html);
       });
     }).on('error', function () {
-      console.log('出错！');
+      reject();
     });
   });
 }
 
-function writeFile(pathInfo, data, options = {flag: 'w+'}) {
+function writeFile(pathInfo, data, options = { flag: 'w+' }) {
   fs.writeFileSync(pathInfo, data, options);
 }
 
-getRemoteData('https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5');
+getRemoteData(httpUrl, staticYiqingJsonPathInfo);
